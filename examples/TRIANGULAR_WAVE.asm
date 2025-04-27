fb438690fddcb6eb874a208855f9319035c1af41
Yousef5962 <yalaa5962@gmail.com>
2025-04-28 00:26:05 +0300

    Add files via upload

---
+++ examples/TRIANGULAR_WAVE.asm
@@ -0,0 +1,32 @@
+CODE SEGMENT
+PORTA  EQU 00H
+Config EQU 06H
+ORG 0
+MOV DX, Config
+MOV AL, 80H
+OUT DX, AL
+MOV DX, PORTA
+
+;PUT YOUR CODE HERE
+START:  XOR AL,AL
+        MOV DX, PORTA
+
+J1:     OUT DX, AL ; Output data to DAC0808
+        INC AL ; AL+1
+
+        CMP AL,0FFH ; Maximum value (FFH)?
+        JNZ J1 ; If not, Jump to J1
+
+
+J2:     OUT DX, AL ; Output data to DAC0808
+        DEC AL ; AL-1
+        AND AL,AL ; Minimum Value (00H)?
+        JNZ J2 ; If not, Jump to J2
+        JMP J1 
+
+
+
+CODE ENDS
+
+
+
---
+++ examples/sawtooth.asm
@@ -0,0 +1,19 @@
+CODE SEGMENT
+PORTA  EQU 00H
+Config EQU 06H
+ORG 0
+MOV DX, Config
+MOV AL, 80H
+OUT DX, AL
+MOV DX, PORTA
+
+;PUT YOUR CODE HERE
+START:  MOV AL, 00H
+j1:     OUT DX,AL
+        INC AL
+        CMP AL,0FFH
+        JNZ j1                 
+        
+        JMP START
+CODE    ENDS
+END
